ac26a0057899b59c56c428923766765663843705
palwashamansoor2002 <palwashamansoor2002@gmail.com>
2025-09-28 00:20:03 +0500

    merged branches

---
+++ db/migrate/20250927190618_create_likes.rb
@@ -0,0 +1,12 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.references :user, foreign_key: true, null: false
+      t.references :likeable, polymorphic: true, null: false
+
+      t.timestamps
+    end
+        add_index :likes, [:user_id, :likeable_id, :likeable_type], unique: true
+
+  end
+end
