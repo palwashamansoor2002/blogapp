2ffcb6df4223f86bbef8b437a1db81de1b09c675
palwashamansoor2002 <palwashamansoor2002@gmail.com>
2025-09-28 13:05:54 +0500

    added association to the posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :likes, as: :likeable, dependent: :destroy
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :body, presence: true
 end
