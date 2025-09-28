46edb197653be89e2d1c68a18229c6a028978e3b
palwashamansoor2002 <palwashamansoor2002@gmail.com>
2025-09-28 23:40:09 +0500

    added the posts contoller and generated view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,56 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  after_action :verify_authorized, except: [:index, :show], unless: :devise_controller?
+
   def index
+    @posts = Post.includes(:user).order(created_at: :desc)
   end
 
   def show
   end
 
   def new
+    @post = current_user.posts.new
+    authorize @post
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    authorize @post
+    if @post.save
+      redirect_to @post, notice: "Post created."
+    else
+      render :new
+    end
   end
 
   def edit
+    authorize @post
+  end
+
+  def update
+    authorize @post
+    if @post.update(post_params)
+      redirect_to @post, notice: "Post updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    authorize @post
+    @post.destroy
+    redirect_to posts_path, notice: "Post deleted."
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
