0fbfea2955042ad343e655b649973b4dd3c93d10
palwashamansoor2002 <palwashamansoor2002@gmail.com>
2025-09-28 13:11:38 +0500

    added associations to the likes and unique likeable type for it

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true
+
+  validates :user_id, presence: true
+  validates :likeable_id, presence: true
+  validates :user_id, uniqueness: { scope: [:likeable_type, :likeable_id], message: "has already liked this" }
 end
